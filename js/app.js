88528268b4d7de75f39e6212530ba0055dc9c371
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-11 16:09:30 +0330

    create sidebar complate

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const sidebar = document.getElementById("sidebar");
+const btn = document.getElementById('trigger_btn');
+const menuText = document.getElementsByClassName("menu_text");
+
+let sidebarActive = true ;
+
+btn.addEventListener("click" , ()=>{
+    if(sidebarActive){
+        sidebar.style.width = '100px';
+        sidebarActive = false;
+        for(let i = 0 ;i< menuText.length; i++){
+            menuText[i].style.display = 'none'
+        }
+    }else{
+        sidebar.style.width = '250px'
+        sidebarActive = true;
+        for(let i = 0 ; i< menuText.length ;i++){
+            menuText[i].style.display  = 'flex'
+        }
+    }
+})
